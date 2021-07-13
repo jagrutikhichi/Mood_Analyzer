b420555610b7dacbf0e6622cb2e6192b7fce6906
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-07-13 22:19:14 +0530

    Added: Happy or Sad Mood Message

--- src/main/java/MoodAnalyser/MoodAnalyzer.java
+++ src/main/java/MoodAnalyser/MoodAnalyzer.java
@@ -2,6 +2,18 @@ package MoodAnalyser;
 
 public class MoodAnalyzer {
 
+	
+	String message;
+
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+	
+
+	public MoodAnalyzer() {
+	}
+
+
 	public String analyzeMood(String message) {
 
 		if (message.contains("SAD"))
--- src/test/java/MoodAnalyser/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyser/MoodAnalyzerTest.java
@@ -6,14 +6,14 @@ import org.junit.Test;
 public class MoodAnalyzerTest {
 	@Test
 	public void givenMood_WhenHappy_ShouldReturnHappy() {
-		MoodAnalyzer moodAnalyser = new MoodAnalyzer();
+		MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in Happy Mood");
 		String mood = moodAnalyser.analyzeMood("HAPPY");
 		// assertTrue( true );
 		Assert.assertEquals("HAPPY", mood);	
 		}
 	@Test
 	public void givenMood_WhenSAD_ShouldReturnSAD() {
-		MoodAnalyzer moodAnalyser = new MoodAnalyzer();
+		MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in Sad Mood");
 		String mood = moodAnalyser.analyzeMood("SAD");
 		// assertTrue( true );
 		Assert.assertEquals("SAD", mood);	
